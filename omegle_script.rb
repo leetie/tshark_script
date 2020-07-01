8d8f68854d96d7184fa916c91848ccfe2f9cd969
Jesse Parsons <savvyjesse@aol.com>
2020-07-01 12:30:01 -0400

    add nokogiri gem for STDOUT interface instead of web browser

--- omegle_script.rb
+++ omegle_script.rb
@@ -1,24 +1,43 @@
 require 'open3'
+require 'nokogiri'
+require 'open-uri'
+require 'colorize'
 
 #script to automate ip lookup through tshark utility and query website
-
+running = true
+system 'clear'
 #set hardcode_ip to local ip address, or let script do it (unreliable)
+while running
+    system 'clear'
+    hardcode_ip = "192.168.1.226"
+    script_ip = Open3.capture3("ip address")[0].split("\n")
+    script_ip = script_ip[-4].strip.split(" ")[1][0..-4]
+    ip_ary = Open3.capture3("sudo tshark -i wlp39s0 -c 10 udp")
+    ary =  ip_ary[0].split(" ")
 
-hardcode_ip = "192.168.1.226"
-script_ip = Open3.capture3("ip address")[0].split("\n")
-script_ip = script_ip[-4].strip.split(" ")[1][0..-4]
-ip_ary = Open3.capture3("sudo tshark -i wlp39s0 -c 10 udp")
-ary =  ip_ary[0].split(" ")
+    #enumerate through ary (array of 10 udp ip's) and if any do not equal my_ip, set variable 
 
-#enumerate through ary (array of 10 udp ip's) and if any do not equal my_ip, set variable 
-
-other_ip = "" 
-ary.each do |i|
-  if i != script_ip
-    if (i[0..2] == "192")
-      next
+    other_ip = "" 
+    ary.each do |i|
+      if i != script_ip
+        if (i[0..2] == "192")
+          next
+        end
+        other_ip = i
+      end
     end
-    other_ip = i
-  end
+    puts other_ip
+    doc = Nokogiri::HTML(open("https://whatismyipaddress.com/ip/#{other_ip}"))
+    doc.search('table').each do |t|
+      puts t.content.red
+    end
+    puts 'press y for next, q for quit'.cyan
+    quit = 'q'
+    continue = 'y'
+    input = gets.chomp
+    if input == continue
+      next
+    else
+      running = false
+    end
 end
-system ("firefox --new-window https://whatismyipaddress.com/ip/#{other_ip}")
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 just a script to capture ips from udp connections using tshark.
 it works locally through grabbing a public ip via system("ip address"), but can be configured with a hard-coded local ip by changing the script_ip variable to the output that you would like to monitor.
 
-the script then enumerates through the ip's captured from 10 packets and if one does not begin with 192, or is not equal to your local ip, sets it to a variable. once eof is reached it opens a firefox tab [https://whatismyipaddress.com/ip/] and appends the variable to it.
+the script then enumerates through the ip's captured from 10 packets and if one does not begin with 192, or is not equal to your local ip, sets it to a variable. uses the (nokogiri)[https://nokogiri.org/] gem to parse a url with the target ip appended to it, and prints info to STDOUT
